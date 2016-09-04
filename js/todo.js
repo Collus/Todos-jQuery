ffe55972eaa242653a1cffa009859cbe95240c2f
Collus <colluswang@163.com>
2016-09-04 23:13:36 +0800

    complete basic functions

--- js/todo.js
+++ js/todo.js
@@ -4,18 +4,19 @@ $(document).ready(function () {
 						if(event.keyCode==13){
 							var val=$('.hdrinput').val();
 							if(val){
-								addTodos();
-								$('.hdrinput').val('');
+								addTodos();//添加新项目
+								$('.hdrinput').val('');//
 								addMouseEvent();
 								delTodos();
 								todoChecked();
+								
 							}
 						}
 			})
 	});
-	//添加新项目，并判断是否为第一次添加
 	var Linum = 0;//统计添加的item
 	var Checked = 0;//统计选中的item
+	//添加新项目，如果是第一次添加，则footer栏也一并添加
 	function addTodos() {
 		if(Linum){
 			addTodoBody();
@@ -27,25 +28,22 @@ $(document).ready(function () {
 		}
 	}
 	//*添加新的项目
-
 	function addTodoBody() {
-		var $newLi = $("<li><input class='chkbox' type='checkbox' ></li>");
-		var $newSpan = $("<span class=''></span>");
+		var $newLi = $("<li><input class='chkbox' type='checkbox' value='unchecked'></li>");
+		var $newSpan = $("<span class='content'></span>");
 		var	$newimg = $("<img class='del' src='images/del.png'>")
 		$newSpan.html($('.hdrinput').val());
 		$('.itemList').append($newLi);
 		$newSpan.appendTo($newLi);
 		$newimg.appendTo($newLi);
 		Linum++;	
+		changeItemStatus();
 	}
-	//添加li的鼠标移动事件，以及元素删除
+	//添加li的鼠标移动事件
 	function addMouseEvent() {
 		$('li').each(function(index) {
 			$(this).mouseover(function () {
 				$(this).children('img').addClass('del1');
-				// $(this).children('img').click(function () {
-				// 	$(this).parent().remove();
-				// });
 			})
 			$(this).mouseout(function () {
 				$(this).children('img').removeClass('del1');
@@ -53,40 +51,87 @@ $(document).ready(function () {
 		});
 	}
 	//删除项目。
-	// function delTodos() {
-	// 	$('li').each(function (index) {
-	// 		$(this).children('img').click(function () {
-	// 			Linum--;console.log(Linum);
-	// 			$(this).parent().remove();
-	// 		})
-	// 	})
-	// }
 	function delTodos() {
-		$('.del').click(function() {
+		$('.del').unbind('click').click(function() { //在对新的‘.del’集合绑定click事件前，先清除老的’.del‘集合绑定的click事件。
 			$(this).parent().remove();
-			console.log(Linum);
+			$(this).parent().next().children('img').addClass('del1');
+			//*如果删除的是未完成的项目，itemleft数目减一;如果删除已完成项目，itemleft不变。
+			if(!$(this).parent().children('.chkbox').prop('checked')) {
+			 	Linum--;
+			 	changeItemStatus();
+			}else
+			{
+				changeItemStatus();
+			}
+			
+			
 		});
 	}
 	//*判断项目是否被选中
 	function todoChecked() {		
-			console.log('chkbox: '+$('.chkbox').size());
-			$('.chkbox').click(function(){
+			$('.chkbox').unbind('click').click(function(){
 				if ($(this).prop('checked')) {
 					$(this).next().css({'text-decoration':'line-through','color':'#ccc'});
-					Checked = $(".chkbox[checked='true']").size();
+					$(this).prop('value','checked');
+					Checked++;
 					console.log(Checked);
+					changeItemStatus();
+					$('#clear').addClass('clear1');//显示Clear completed按钮
+					showActive();
+					showAll();
+					showCompleted();
+					clearCompleted();
 				} else {
 					$(this).next().removeAttr('style');	
+					$(this).prop('value','unchecked');
+					Checked--;
+					console.log(Checked);
+					changeItemStatus();
+					if (Checked==0) {
+						$('#clear').removeClass('clear1');//隐藏Clear completed按钮
+					}
+					showActive();
+					showAll();
+					showCompleted();
+					clearCompleted();
 				}
 			})
 			
+		}
+	//*显示未完成的项目个数
+	function changeItemStatus() {
+			$('#unchecked').html(Linum - Checked);
 		} 
-	//*统计Checked的条目数
-	// function countChecked() {
-	// 	Checked = 0;
-	// 	Checked = $(".chkbox[checked=true]").size();
-	// 	console.log(Checked);
-	// }
+	//显示所有未完成的项目。用checkbox的value使用进行筛选
+	function showActive() {
+		$('#active').click(function () {
+			$(".chkbox[value='checked']").parent().addClass('lihidden');
+			$(".chkbox[value='unchecked']").parent().removeClass('lihidden');
+		})
+		}
+	//显示所有项目
+	function showAll() {
+		$('#all').click(function () {
+			$(".chkbox[value='checked']").parent().removeClass('lihidden');
+			$(".chkbox[value='unchecked']").parent().removeClass('lihidden');
+		})
+		}
+	//显示所有完成的项目
+	function showCompleted() {
+		$('#completed').click(function () {
+			$(".chkbox[value='unchecked']").parent().addClass('lihidden');
+			$(".chkbox[value='checked']").parent().removeClass('lihidden');
+		})
+		}	
+	//删除所有已完成的项目
+	function clearCompleted() {
+		$('.clear').click(function () {
+			$(".chkbox[value='checked']").parent().remove();
+			$(".chkbox[value='checked']").prop('value',unchecked);
+
+		})
+		}
+
 
 
 })
